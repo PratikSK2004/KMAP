39a0705255b86b2765a54f5be282bdcd49f7284e
PratikSK2004 <1nt23is164.pratik@nmit.ac.in>
2025-01-04 10:59:16 +0530

    the python file has been added

--- name.py
+++ name.py
@@ -1,7 +1,1 @@
-def calculate_rectangle_area(length, width):
-    return length * width
-
-# Example usage
-length = 5
-width = 10
-print("Area of the rectangle:", calculate_rectangle_area(length, width))
+"print('new python file has been created')" 
