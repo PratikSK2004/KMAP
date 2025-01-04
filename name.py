adadf850c88dc5b810767ff71250f75598f6a375
PratikSK2004 <1nt23is164.pratik@nmit.ac.in>
2025-01-04 10:58:21 +0530

    added successfully

---
+++ name.py
@@ -0,0 +1,7 @@
+def calculate_rectangle_area(length, width):
+    return length * width
+
+# Example usage
+length = 5
+width = 10
+print("Area of the rectangle:", calculate_rectangle_area(length, width))
